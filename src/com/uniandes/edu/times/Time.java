25a98729aa65d10bfdc6fc7efac73d3b7ef79e31
Juan Guevara <soyjuandavidguevara@gmail.com>
2024-11-05 19:54:33 -0500

    Now time supports name

--- src/com/uniandes/edu/times/Time.java
+++ src/com/uniandes/edu/times/Time.java
@@ -6,6 +6,13 @@ import java.time.temporal.ChronoUnit;
 public class Time {
   private final LocalDateTime startTime = LocalDateTime.now();
   private LocalDateTime endTime;
+  public String name = "";
+
+  public Time() { }
+
+  public Time(String name) {
+    this.name = name + "=";
+  }
 
   public void close() {
     this.endTime = LocalDateTime.now();
@@ -17,6 +24,6 @@ public class Time {
 
   @Override
   public String toString() {
-    return nanoseconds() + "ns";
+    return name + nanoseconds() + "ns";
   }
 }
